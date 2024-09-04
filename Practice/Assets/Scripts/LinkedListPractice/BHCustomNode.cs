c8cdc4a9dc3abab04ef799c90d7683182b44afa5
yyy <yyy971015@playhardlab.com>
2024-09-04 14:29:10 +0900

    에디터 주석

--- Practice/Assets/Scripts/LinkedListPractice/BHCustomNode.cs
+++ Practice/Assets/Scripts/LinkedListPractice/BHCustomNode.cs
@@ -1,27 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class BHCustomNode : MonoBehaviour
-{
-    [SerializeField] TextMeshProUGUI txt_Index;
+//public class BHCustomNode : MonoBehaviour
+//{
+//    [SerializeField] Button btn_DestroyNode;
+//    [SerializeField] TextMeshProUGUI txt_Index;
+//    CustomLinkedListSceneGameManager manager;
+//    public int index;
 
-    public int index;
+//    void Start()
+//    {
+//        btn_DestroyNode.onClick.AddListener(OnClickDestroyNode);
+//    }
 
-    void Start()
-    {
-    }
+//    public void SetIndex(int index, CustomLinkedListSceneGameManager manager)
+//    {
+//        this.index = index;
+//        txt_Index.text = index.ToString();
+//        this.manager = manager;
+//    }
 
-    public void SetIndex(int index)
-    {
-        this.index = index;
-        txt_Index.text = index.ToString();
-    }
+//    public void Refresh()
+//    {
+//        txt_Index.text = index.ToString();
+//    }
 
-    public void Refresh()
-    {
-        txt_Index.text = index.ToString();
-    }
+//    void OnClickDestroyNode()
+//    {
+//        manager.DestroyNode(index);
+//        Destroy(this.gameObject);
+//    }
 
-}
+//}
--- Practice/Assets/Scripts/LinkedListPractice/CustomLinkedListSceneGameManager.cs
+++ Practice/Assets/Scripts/LinkedListPractice/CustomLinkedListSceneGameManager.cs
@@ -1,75 +1,71 @@
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
-using YYY;
+//using System.Threading;
+//using System;
+//using TMPro;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using YYY;
 
-public class CustomLinkedListSceneGameManager : MonoBehaviour
-{
-    #region inspector
-    [SerializeField] Button btn_GenerateNode;
-    [SerializeField] Button btn_DestroyNode;
-    [SerializeField] Transform trans_NodePivot;
-    [SerializeField] TMP_InputField txt_inputField;
-    #endregion
-
-
-    #region variables
-    CustomLinkedList<BHCustomNode> list = new CustomLinkedList<BHCustomNode>();
-    #endregion
+//public class CustomLinkedListSceneGameManager : MonoBehaviour
+//{
+//    #region inspector
+//    [SerializeField] Button btn_GenerateNode;
+//    [SerializeField] Transform trans_NodePivot;
+//    [SerializeField] TMP_InputField txt_inputField;
+//    #endregion
 
 
-
-    #region overriden Methods
-    void Start()
-    {
-        btn_GenerateNode.onClick.AddListener(OnClickGenerateNode);
-        btn_DestroyNode.onClick.AddListener(OnClickDestroyNode);
-    }
-    #endregion
-
-    #region private methods
-
-    #endregion
-
-    void RefreshNodes()
-    {
-        for(int i = 0; i < list.GetCount(); ++i)
-        {
-            list.Get(i).Refresh();
-        }
-    }
+//    #region variables
+//    CustomLinkedList<BHCustomNode> list = new CustomLinkedList<BHCustomNode>();
+//    #endregion
 
 
-    #region callBack
 
-    void OnClickGenerateNode()
-    {
-        var clone = Resources.Load<BHCustomNode>("LinkedListPractice/CustomNode");
-        if (clone == null)
-            return;
+//    #region overriden Methods
+//    void Start()
+//    {
+//        btn_GenerateNode.onClick.AddListener(OnClickGenerateNode);
+//    }
+//    #endregion
 
-        var node = Instantiate(clone, trans_NodePivot);
-        if (node == null)
-            return;
+//    #region private methods
 
-        var txt = txt_inputField.text;
-        var idx = int.Parse(txt);
+//    #endregion
 
-        node.SetIndex(idx);
-        list.AddElement(node);
+//    void RefreshNodes()
+//    {
+//        for(int i = 0; i < list.GetCount(); ++i)
+//        {
+//            list.Get(i).Refresh();
+//        }
+//    }
 
-        RefreshNodes();
-    }
-    void OnClickDestroyNode()
-    {
-        var txt = txt_inputField.text;
-        var RemoveIdx = int.Parse(txt);
-        var value = list.Get(RemoveIdx);
 
-        list.RemoveElement(value);
+//    #region callBack
 
-        RefreshNodes();
-    }
+//    void OnClickGenerateNode()
+//    {
+//        var clone = Resources.Load<BHCustomNode>("LinkedListPractice/CustomNode");
+//        if (clone == null)
+//            return;
 
-    #endregion
-}
+//        var node = Instantiate(clone, trans_NodePivot);
+//        if (node == null)
+//            return;
+
+//        var txt = txt_inputField.text;
+//        var idx = int.Parse(txt);
+         
+//        node.SetIndex(idx, this);
+//        list.AddElement(node);
+
+//        RefreshNodes();
+//    }
+//    public void DestroyNode(int idx)
+//    {
+//        var value = list.Get(idx);
+//        list.RemoveElement(value);
+//        RefreshNodes();
+//    }
+
+//    #endregion
+//}
